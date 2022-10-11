691efb39deeaa3abd741743e966d108b3b7587b9
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-10-11 19:00:54 +0300

    e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Profile } from "components/Profile/Profile";
+import user from './Data/user'
+
+// import Statistics from './components/Statistics/Statistics';
+// import statisticalData from './data/statistical-data.json';
+
+// import FriendList from './components/FriendList/FriendList';
+// import friends from './data/friends.json';
+
+// import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+// import transactions from './data/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework templatehhh
+    <div>
+      < Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}/>
     </div>
+    // <Statistics title="Upload stats" stats={statisticalData} />
+    //   <Statistics stats={statisticalData} />
+
+    //   <FriendList friends={friends} />
+
+    //   <TransactionHistory items={transactions} />
   );
 };
